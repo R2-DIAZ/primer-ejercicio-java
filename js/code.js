795b65b9ff250a0d2014354abc603d7e6a4753e2
Arturo Diaz <ceroabsoluto@outlook.com>
2023-10-07 13:20:32 -0700

    primer ejercicio java

---
+++ js/code.js
@@ -0,0 +1,76 @@
+//let nombre = "Juan";
+//let apellido = "González"
+//let ciudadResidencia = "Monterrey"
+//let estadoCivil = "Soltero"
+//let edad = 50;
+//let estaVivo = true;
+
+//console.log("El nombre es "+ nombre);
+//console.log(`Mi nombre es: ${nombre} ${apellido} y soy de: ${ciudadResidencia}`);
+
+
+
+//Ejercicio 1//
+
+//let nombre1 = "Arturo";
+//let apellido1 = "Díaz";
+//let edad1 = "36";
+//let soltero = false;
+//let fechadenacimiento= "05 de junio de 1987";
+
+
+//console.log (`Mi nombre es: ${nombre1} ${apellido1}`);
+//console.log (`Y tengo ${edad1} años`); 
+//console.log (`Y nací el ${fechadenacimiento}`);
+//console.log (`¿Soltero? ${soltero}`);
+
+
+//Coerción de datos
+//let num1= "10";
+//let num2 = 5;
+
+//let suma= (Number(num1)+num2);
+
+//console.log(suma);
+
+
+// OPERADORES
+
+//let division = 11/3;
+//let residuo = 11 % 3;
+
+//console.log(division);
+//console.log(residuo);
+
+//console.log(3==="3");
+
+//let num1= 10;
+//let num2= 20;
+
+//let suma=(num1 + num2);
+//let resta=(num1 - num2);
+//let multiplicacion=(num1 * num2);
+//let division=(num1 / num2);
+
+//console.log(`Esta es la suma: ${suma}`);
+//console.log(`Esta es la resta: ${resta}`);
+//console.log(`Esta es la multiplicación: ${multiplicacion}`);
+//console.log(`Esta es la division: ${division}`);
+
+//EJERCICIO DE IF-ELSE
+
+let num1= 10;
+let num2= 20;
+
+console.log (`Número 1: ${num1}`);
+console.log (`Número 2: ${num2}`);
+
+
+if (num1>num2) {
+    console.log(`${num1} es mayor a ${num2}`)
+} else if (num1<num2){
+    console.log(`${num1} es menor a ${num2}`)
+    
+} else {
+    console.log(`${num1} es igual a ${num2}`)
+}
